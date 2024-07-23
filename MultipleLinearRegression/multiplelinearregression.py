9d51351deae36fc132051bb7b0760865a7022e53
Mriganka <pmriganka707@gmail.com>
2024-07-23 17:32:03 +0530

    office laptop commit 1

---
+++ MultipleLinearRegression/multiplelinearregression.py
@@ -0,0 +1,9 @@
+'''
+Assumptions of Linear Regression
+- Linearity (Linear Relationship between Y and each X)
+- Homoscedasticity (Equal variance , there should not be any cone at any end)
+- Multivariate Normality (Normality of error distribution)
+- Independence (of observations includes "no autocorrelation ie htere should not any pattern in data that you can see)
+- Lack of Multicollinearity ( predictors are npot corelated to each other)
+- The Outlier Check ( Not assumptions but extra)
+'''
