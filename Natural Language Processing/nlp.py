b874997399765cab13e3183cc83ef6bd1c676bbf
Mriganka <pmriganka707@gmail.com>
2024-09-14 20:14:12 +0530

    htftf

---
+++ Natural Language Processing/nlp.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+#CLEANING THE TEXTs
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+  review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+  review = review.lower()
+  review = review.split()
+  ps = PorterStemmer()
+  all_stopwords = stopwords.words('english')
+  all_stopwords.remove('not')
+  review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+  review = ' '.join(review)
+  corpus.append(review)
+  
+#CREATING THE BAG OF WORDS MODEL
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, -1].values
+
+#SPLITTING THE DATASET INTO THE TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#TRAINING THE NAIVE BAYES MODEL ON THE TRAINING SET
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+#PREDICTING THE TEST SET RESULTS
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+#MaKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
+
+'''
+Positive review
+Use our model to predict if the following review:
+"I love this restaurant so much"
+is positive or negative.
+Solution: We just repeat the same text preprocessing process we did before, but this time with a single review.
+
+OUTPUT: [1]
+'''
+new_review = 'I love this restaurant so much'
+new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+new_review = new_review.lower()
+new_review = new_review.split()
+ps = PorterStemmer()
+all_stopwords = stopwords.words('english')
+all_stopwords.remove('not')
+new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
+new_review = ' '.join(new_review)
+new_corpus = [new_review]
+new_X_test = cv.transform(new_corpus).toarray()
+new_y_pred = classifier.predict(new_X_test)
+print(new_y_pred)
+
+'''
+Negative review
+Use our model to predict if the following review:
+"I hate this restaurant so much"
+is positive or negative.
+Solution: We just repeat the same text preprocessing process we did before, but this time with a single review.
+
+OUTPUT: [0]
+'''
+
+new_review = 'I hate this restaurant so much'
+new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+new_review = new_review.lower()
+new_review = new_review.split()
+ps = PorterStemmer()
+all_stopwords = stopwords.words('english')
+all_stopwords.remove('not')
+new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
+new_review = ' '.join(new_review)
+new_corpus = [new_review]
+new_X_test = cv.transform(new_corpus).toarray()
+new_y_pred = classifier.predict(new_X_test)
+print(new_y_pred)
+
+
+'''
+If you are up for some practical activities, here is a little challenge:
+
+1. Run the other classification models we made in Part 3 - Classification, other than the one we used in the last tutorial.
+2. Evaluate the performance of each of these models. Try to beat the Accuracy obtained in the tutorial. But remember, Accuracy is not enough, so you should also look at other performance metrics like Precision (measuring exactness), Recall (measuring completeness) and the F1 Score (compromise between Precision and Recall). Please find below these metrics formulas (TP = # True Positives, TN = # True Negatives, FP = # False Positives, FN = # False Negatives):
+
+Accuracy = (TP + TN) / (TP + TN + FP + FN)
+Precision = TP / (TP + FP)
+Recall = TP / (TP + FN)
+F1 Score = 2 * Precision * Recall / (Precision + Recall)
+
+3. Try even other classification models that we haven't covered in Part 3 - Classification. Good ones for NLP include:
+
+CART
+C5.0
+Maximum Entropy
+'''
+
+
+
